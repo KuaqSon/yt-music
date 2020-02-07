984000ff5c1a2c6bf37e1fccf36eb1e987cc7ae5
KuaqSon <qson.ng@gmail.com>
2020-02-07 18:04:39 +0700

    add play pause button

--- src/components/Player.js
+++ src/components/Player.js
@@ -7,6 +7,7 @@ export default function Player({ src, name }) {
   const [played, setPlayed] = useState(0);
   const [loaded, setLoaded] = useState(0);
   const [pip, setPip] = useState(false);
+  const [playing, setPlaying] = useState(true);
 
   const ref = player => {
     setPlayer(player);
@@ -25,6 +26,14 @@ export default function Player({ src, name }) {
     setPlayed(state.played);
   };
 
+  const handlePlay = () => {
+    setPlaying(true);
+  };
+
+  const handlePause = () => {
+    setPlaying(false);
+  };
+
   return (
     <div className="plyer_panel">
       <div className="card acrylic">
@@ -33,6 +42,10 @@ export default function Player({ src, name }) {
           <audio id="youtube" autoPlay={true} controls={true} loop={true} src={src}></audio>
         </div> */}
 
+        <div>
+          {!playing && <button onClick={() => handlePlay()}>Play</button>}
+          {playing && <button onClick={() => handlePause()}>Pause</button>}
+        </div>
         <div>
           <label>Seek</label>
           <div>
@@ -45,12 +58,14 @@ export default function Player({ src, name }) {
               // onMouseDown={this.handleSeekMouseDown}
               onChange={handleSeekChange}
               onMouseUp={handleSeekMouseUp}
+              onPlay={handlePlay}
+              onPause={handlePause}
             />
           </div>
         </div>
 
         <div style={{ display: "none" }}>
-          <ReactPlayer ref={ref} url={src} onProgress={handleProgress} playing />
+          <ReactPlayer ref={ref} url={src} onProgress={handleProgress} playing={playing} />
         </div>
       </div>
     </div>
